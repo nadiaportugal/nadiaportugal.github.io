49d514f1e0f44305e33005b97144d4f612399ea1
Nadia Faya <Nadia Faya>
2016-04-20 17:34:57 -0300

    Arreglando lo que rompí de derecho

--- pisculichi.js
+++ pisculichi.js
@@ -5,9 +5,11 @@
   $(document).on("click", ".js-nico3", function(event){
 
     var $self = $(this);
+    var $column = $self.closest(".col-sm-4");
     var $container = $self.closest(".js-greetings");
     $container.find(".col-sm-4").fadeOut(function(){
-      $self.removeClass("col-sm-4").addClass("col-sm-12 text-center").fadeIn();
+      $column.find(".panel").not($self).hide();
+      $column.removeClass("col-sm-4").addClass("col-sm-12 text-center").fadeIn();
     });
 
   })
